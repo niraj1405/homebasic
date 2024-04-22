21fa4dd9834e5f0e265426d8c28d9c2102840fee
niraj1405 <89933516+niraj1405@users.noreply.github.com>
2024-04-22 16:19:10 +0530

    Delete javatechie4 directory

--- javatechie4/src/main/java/springcrudwithmysql/dao/TicketDao.java
+++
@@ -1,9 +0,0 @@
-package springcrudwithmysql.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import springcrudwithmysql.model.Ticket;
-
-public interface TicketDao extends CrudRepository<Ticket, Integer>{
-
-}
