187afd8028b89bb6ed0ae7e7ed1bd2394884f939
niraj1405 <89933516+niraj1405@users.noreply.github.com>
2024-04-22 16:18:56 +0530

    Delete TicketingApp directory

--- TicketingApp/src/main/java/com/app/webapp/dao/TicketDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.webapp.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.app.webapp.model.Ticket;
-
-public interface TicketDao extends CrudRepository<Ticket, Integer>{
-
-}
